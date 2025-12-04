a7d989614f995d9937a95c77b63c1c7be0808394
kaledOu <kaled.ouali@gmail.com>
2025-12-04 12:45:24 +0100

    fix coverage
    Signed-off-by: kaledOu <kaled.ouali@gmail.com>

---
+++ kernel/kernel-otpmanager-service/src/test/java/io/mosip/kernel/otpmanager/test/util/PasscodeGeneratorTest.java
@@ -0,0 +1,30 @@
+package io.mosip.kernel.otpmanager.test.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import io.mosip.kernel.otpmanager.exception.OtpIOException;
+import io.mosip.kernel.otpmanager.util.PasscodeGenerator;
+
+public class PasscodeGeneratorTest {
+
+    @Test
+    public void testPadOutputAddsLeadingZerosViaGenerate() throws Exception {
+        // Create signer that returns 5 bytes: first 4 bytes = int 123, last byte (offset) = 0
+        PasscodeGenerator.Signer signer = data -> new byte[] { 0, 0, 0, 123, 0 };
+        PasscodeGenerator pc = new PasscodeGenerator(signer, 6);
+        String out = pc.generateResponseCode(new byte[] { 0 });
+        // generateResponseCode should produce pinValue 123 and pad to 6 digits
+        assertEquals("000123", out);
+    }
+
+    @Test(expected = OtpIOException.class)
+    public void testHashToIntIOExceptionIsWrappedAsOtpIOException() throws Exception {
+        // Signer returns a single byte; offset will be 1 -> ByteArrayInputStream length 0 -> readInt throws EOFException
+        PasscodeGenerator.Signer signer = data -> new byte[] { 1 };
+        PasscodeGenerator pc = new PasscodeGenerator(signer, 6);
+        // Call generateResponseCode with any input; signer.sign will return single-byte array and cause hashToInt to throw
+        pc.generateResponseCode(new byte[] { 0 });
+    }
+}
